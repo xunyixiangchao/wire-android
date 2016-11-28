56a70767f45998889d2e0c1f77b77c35851c1391
Shumeng <shumeng.ye@wearezeta.com>
2016-11-28 16:57:52 +0100

    [AN-4691] Visual tweaks to header view in participants Ticket: https://wearezeta.atlassian.net/browse/AN-4691

--- app/src/main/java/com/waz/zclient/pages/main/connect/PendingConnectRequestFragment.java
+++ app/src/main/java/com/waz/zclient/pages/main/connect/PendingConnectRequestFragment.java
@@ -494,6 +494,9 @@ public class PendingConnectRequestFragment extends BaseFragment<PendingConnectRe
     }
 
     private void updateToolbarNavigationIcon(Configuration newConfig) {
+        if (LayoutSpec.isPhone(getContext())) {
+            return;
+        }
         if (ViewUtils.isInLandscape(getContext()) ||
             (newConfig != null && ViewUtils.isInLandscape(newConfig))) {
             toolbar.setNavigationIcon(null);
